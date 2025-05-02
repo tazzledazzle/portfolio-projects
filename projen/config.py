e4c8d1937d6c16219eb36be73c9268276dfa6446
Terence Schumacher <terenceschumacher@gmail.com>
2025-05-01 17:16:11 -0700

    ^ f created configuration for command line options. This allows a default map of common configurations

---
+++ projen/config.py
@@ -0,0 +1,14 @@
+import os
+
+DEFAULTS = {
+    # Default build system: bazel, gradle, or both
+    'build': 'both',
+    # Supported languages
+    'languages': ['java', 'kotlin', 'groovy', 'cpp', 'c', 'python', 'rust', 'node', 'typescript'],
+    # CI providers
+    'ci_providers': ['github'],
+    # Default license
+    'license': 'MIT',
+    # Directory where Jinja2 templates reside
+    'templates_dir': os.path.join(os.path.dirname(__file__), 'templates')
+}
