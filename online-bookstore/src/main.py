85e1fa693511fee124466a3120359c3a19d080d4
Terence Schumacher <terenceschumacher@gmail.com>
2025-07-08 10:38:09 -0700

    added root endpoint and exception handler

--- online-bookstore/src/main.py
+++ online-bookstore/src/main.py
@@ -7,4 +7,26 @@ from models import (
     OrderWithID,
     OrderItems,
 )
+app = FastAPI()
 
+
+
+
+# Root endpoint
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Online Bookstore API"}
+
+
+
+
+
+
+## custom error handler for 404 Not Found
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    return {
+        "status_code": exc.status_code,
+        "detail": exc.detail,
+        "message": "Resource not found"
+    }
