deb9a42726009a75a2a5f446ee7b0822f9edf0b4
Terence Schumacher <terenceschumacher@gmail.com>
2025-07-09 13:11:19 -0700

    added rabbit-mq projects

---
+++ rabbit-mq/src/main/kotlin/com/example/App.kt
@@ -0,0 +1,8 @@
+package com.example
+
+
+fun main() {
+    println("Hello, World!")
+    // This is a simple Kotlin application that prints "Hello, World!" to the console.
+    // You can run this application using the Kotlin command line tools or an IDE like IntelliJ IDEA.
+}
