4c97ccc01daf452d668ba5a2df5f46c5691bb2b8
Terence Schumacher <terenceschumacher@gmail.com>
2025-07-09 10:12:33 -0700

    moved frontend code to subdirectory; created java runner build;

---
+++ bazel-multibuild/java/src/main/java/com/example/ProjectRunner.java
@@ -0,0 +1,9 @@
+package com.example;
+
+// This is a simple Java class that serves as an entry point for the application.
+// It calls the Greeting class to print a greeting message.
+public class ProjectRunner {
+    public static void main(String[] args) {
+        Greeting.sayHi();
+    }
+}
